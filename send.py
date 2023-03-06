f37ffababb9b61a4fc00faac6d77abc73a03aa71
Norman Zhi <normanzyb@gmail.com>
2023-03-06 21:32:25 +0000

    Update send.py to python-telegram-bot v20.1

--- send.py
+++ send.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-import sys, os, subprocess, tempfile
+import sys, os, subprocess, tempfile, asyncio
 from pathlib import Path, PurePath
 from argparse import ArgumentParser
 
 #print('\n'.join(sys.path))
 
-from telegram import ParseMode
-from telegram.ext import Updater
+from telegram.constants import ParseMode
+from telegram.ext import Application
 import logging
 import sys, socket
 
@@ -45,17 +45,17 @@ def send_video(bot, chat, path):
     #thumb_server=f"/data/tmp/{PurePath(thumb).name}"
     #video_data=Path(path).read_bytes()
     thumb_data=Path(thumb).read_bytes()
-    bot.send_video(chat_id=chat,
+    asyncio.run(bot.send_video(chat_id=chat,
             video=video_data, thumb=thumb_data,
             #video=f"file://{server_video}", thumb=thumb_bytes,
             width=info['width'], height=info['height'], duration=round(info['duration']),
-            supports_streaming=True, disable_notification=True, timeout=300)
+            supports_streaming=True, disable_notification=True, timeout=300))
 
     os.system(f"ssh vps rm {video_server}")
     os.remove(thumb)
 
 def send_md(bot, chat, text):
-    bot.send_message(chat_id=chat, text=text, parse_mode=ParseMode.MARKDOWN)
+    asyncio.run(bot.send_message(chat_id=chat, text=text, parse_mode=ParseMode.MARKDOWN))
 
 def main():
     parser = ArgumentParser()
@@ -67,17 +67,14 @@ def main():
     args, msg = parser.parse_known_args()
     print(args)
 
-    updater = Updater(token=token, base_url=base_url, use_context=True)
+    app = Application.builder().token(token).build()
 
     if args.video:
-        send_video(updater.bot, args.chat, args.video)
+        send_video(app.bot, args.chat, args.video)
 
     else:
         text = "_%s_\n%s"%(socket.gethostname(), "\n".join(msg))
-        send_md(updater.bot, args.chat, text)
-
-    #updater.start_polling()
-    #updater.idle()
+        send_md(app.bot, args.chat, text)
 
 if __name__ == '__main__':
     main()
