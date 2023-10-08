b576e45989dbf4c49f5540d5caeaaa188f5afee3
Norman Zhi <normanzyb@gmail.com>
2023-10-08 15:25:23 +0100

    Fix alive check in monitoring

--- alive.py
+++ alive.py
@@ -10,178 +10,209 @@ __all__ = ['register']
 service = services.Service('alive')
 
 class AliveBot:
-	# Message auto delete timer
-	auto_delete_sec = 3
-	# Interval between check in
-	interval = 24 * 60 * 60
-	#interval = 10
-	# Extra delay to wait for response before sending out each public broadcast
-	public_wait = interval / 2
-	# Start sending out public broadcasts after this time (multiples of interval) has elapsed
-	public_interval = interval * 2
-	# No more public broadcasts after this time
-	public_interval_max = public_interval + interval * 7 + public_wait
+    # Message auto delete timer
+    auto_delete_sec = 3
+    # Interval between check in
+    interval = 24 * 60 * 60
+    #interval = 10
+    # Extra delay to wait for response before sending out each public broadcast
+    public_wait = interval / 2
+    # Start sending out public broadcasts after this time (multiples of interval) has elapsed
+    public_interval = interval * 2
+    # No more public broadcasts after this time
+    public_interval_max = public_interval + interval * 7 + public_wait
 
-	def __init__(self, app):
-		self.app = app
-		self.clients = {}
-		for c in service:
-			if 'public' in c:
-				self.new_client(int(c.name))
-		app.add_handler(CommandHandler('alive', self.message))
+    def __init__(self, app):
+        self.app = app
+        self.clients = {}
+        for c in service:
+            if 'public' in c and 'disabled' not in c:
+                self.new_client(int(c.name))
+        app.add_handler(CommandHandler('alive', self.message))
 
-	def new_client(self, chat_id):
-		if chat_id in self.clients:
-			return
-		self.clients[chat_id] = {'client_msgs':[], 'public_msgs':[]}
-		self.app.job_queue.run_once(self.check_client_job, timedelta(seconds=0), chat_id)
+    def new_client(self, chat_id):
+        if chat_id in self.clients:
+            return
+        self.clients[chat_id] = {
+            'client_msgs':[], 'public_msgs':[],
+            'timestamp':datetime.now(timezone.utc).timestamp(),
+        }
+        self.app.job_queue.run_once(self.check_client_job, timedelta(seconds=0), chat_id)
 
-	async def check_client(self, chat_id):
-		utcnow = datetime.now(timezone.utc)
-		client = service[chat_id]
-		if 'public' not in client:
-			return
-		time = datetime.fromtimestamp(float(client['timestamp']), timezone.utc)
-		delta = utcnow - time
-		sec = delta.total_seconds()
-		next = self.interval - sec if sec < self.interval else self.interval
-		self.app.job_queue.run_once(self.check_client_job, timedelta(seconds=next), chat_id)
+    async def check_client(self, chat_id):
+        utcnow = datetime.now(timezone.utc)
+        client = service[chat_id]
+        if 'public' not in client or client['disabled']:
+            del self.clients[chat_id]
+            return
+        time = datetime.fromtimestamp(float(client['timestamp']), timezone.utc)
+        delta = utcnow - time
+        sec = delta.total_seconds()
+        next = self.interval - sec if sec < self.interval else self.interval
+        self.app.job_queue.run_once(self.check_client_job, timedelta(seconds=next), chat_id)
 
-		if sec >= self.public_interval_max:
-			return
+        if sec >= self.public_interval_max:
+            return
 
-		if sec >= self.interval:
-			sdelta = timedelta(seconds=round(sec))
-			text = f"_Beep\\-boop\\!_\nLast checked in *{helpers.escape_markdown(str(sdelta), 2)}* ago\\.\nCheck in? \\/alive"
-			msg = await self.app.bot.send_message(chat_id=chat_id, parse_mode=ParseMode.MARKDOWN_V2, text=text)
-			await self.delete_msgs(self.clients[chat_id]['client_msgs'])
-			self.clients[chat_id]['client_msgs'] = [msg]
+        if sec >= self.interval:
+            sdelta = timedelta(seconds=round(sec))
+            text = f"_Beep\\-boop\\!_\nLast checked in *{helpers.escape_markdown(str(sdelta), 2)}* ago\\.\nCheck in? \\/alive"
+            msg = await self.app.bot.send_message(chat_id=chat_id, parse_mode=ParseMode.MARKDOWN_V2, text=text)
+            await self.delete_msgs(self.clients[chat_id]['client_msgs'])
+            self.clients[chat_id]['client_msgs'] = [msg]
 
-		if sec >= self.public_interval:
-			self.app.job_queue.run_once(self.public_job, timedelta(seconds=self.public_wait), chat_id)
+        if sec >= self.public_interval:
+            self.app.job_queue.run_once(self.public_job, timedelta(seconds=self.public_wait), chat_id)
 
-	async def check_client_job(self, context: CallbackContext):
-		self.check_client(context.job.data)
+    async def check_client_job(self, context: CallbackContext):
+        await self.check_client(context.job.data)
 
-	async def public_job(self, context: CallbackContext):
-		utcnow = datetime.now(timezone.utc)
-		chat_id = context.job.data
-		client = service[chat_id]
-		if 'public' not in client:
-			return
-		time = datetime.fromtimestamp(float(client['timestamp']), timezone.utc)
-		delta = utcnow - time
-		sec = delta.total_seconds()
-		if sec < self.public_interval:
-			return
+    async def public_job(self, context: CallbackContext):
+        utcnow = datetime.now(timezone.utc)
+        chat_id = context.job.data
+        client = service[chat_id]
+        if 'public' not in client or client['disabled']:
+            return
+        time = datetime.fromtimestamp(float(client['timestamp']), timezone.utc)
+        delta = utcnow - time
+        sec = delta.total_seconds()
+        if sec < self.public_interval:
+            return
 
-		msg = await self.public_broadcast(chat_id, int(client['public']))
-		await self.delete_msgs(self.clients[chat_id]['public_msgs'])
-		self.clients[chat_id]['public_msgs'] = [msg]
+        msg = await self.public_broadcast(chat_id, int(client['public']))
+        await self.delete_msgs(self.clients[chat_id]['public_msgs'])
+        self.clients[chat_id]['public_msgs'] = [msg]
 
-	async def public_broadcast(self, chat_id, dst_id):
-		utcnow = datetime.now(timezone.utc)
-		client = service[chat_id]
-		time = datetime.fromtimestamp(float(client['timestamp']), timezone.utc)
-		delta = utcnow - time
-		sec = delta.total_seconds()
+    async def public_broadcast(self, chat_id, dst_id):
+        utcnow = datetime.now(timezone.utc)
+        client = service[chat_id]
+        time = datetime.fromtimestamp(float(client['timestamp']), timezone.utc)
+        delta = utcnow - time
+        sec = delta.total_seconds()
 
-		chat = await self.app.bot.get_chat(chat_id)
-		sdelta = timedelta(seconds=round(sec))
-		custom = client['custom']
-		text = f"""
+        chat = await self.app.bot.get_chat(chat_id)
+        sdelta = timedelta(seconds=round(sec))
+        custom = client['custom']
+        text = f"""
 _Beep\\-boop\\!_
 {helpers.escape_markdown(f"{chat.first_name} {chat.last_name} @{chat.username}", 2)}
 Last seen *{helpers.escape_markdown(str(sdelta), 2)}* ago\\.
 
 {custom.decode('utf8') if custom else ''}
 """.strip()
-		return await self.app.bot.send_message(chat_id=dst_id, parse_mode=ParseMode.MARKDOWN_V2, text=text)
+        return await self.app.bot.send_message(chat_id=dst_id, parse_mode=ParseMode.MARKDOWN_V2, text=text)
 
 
-	async def delete(self, msg):
-		try:
-			await msg.delete()
-		except:
-			pass
+    async def delete(self, msg):
+        try:
+            await msg.delete()
+        except:
+            pass
 
-	async def delete_msgs(self, msgs):
-		for m in msgs:
-			await self.delete(m)
+    async def delete_msgs(self, msgs):
+        for m in msgs:
+            await self.delete(m)
 
-	async def delete_chat_msgs(self, chat_id):
-		await self.delete_msgs(self.clients[chat_id]['public_msgs'])
-		self.clients[chat_id]['public_msgs'] = []
-		await self.delete_msgs(self.clients[chat_id]['client_msgs'])
-		self.clients[chat_id]['client_msgs'] = []
+    async def delete_chat_msgs(self, chat_id):
+        await self.delete_msgs(self.clients[chat_id]['public_msgs'])
+        self.clients[chat_id]['public_msgs'] = []
+        await self.delete_msgs(self.clients[chat_id]['client_msgs'])
+        self.clients[chat_id]['client_msgs'] = []
 
-	async def message(self, update, context):
-		chat_id = update.effective_chat.id
-		client = service[chat_id]
-		client['timestamp'] = datetime.now(timezone.utc).timestamp()
-		if chat_id in self.clients:
-			await self.delete_chat_msgs(chat_id)
+    async def message(self, update, context):
+        chat_id = update.effective_chat.id
+        client = service[chat_id]
+        client['timestamp'] = datetime.now(timezone.utc).timestamp()
+        if chat_id in self.clients:
+            await self.delete_chat_msgs(chat_id)
 
-		query = update.message.text_markdown_v2
-		query = query.split(maxsplit=1)[1:]
-		if not query:
-			msg = await context.bot.send_message(chat_id=chat_id,
-				parse_mode=ParseMode.MARKDOWN_V2, text="_Beep\\-boop\\!_")
+        query = update.message.text_markdown_v2
+        query = query.split(maxsplit=1)[1:]
+        if not query:
+            msg = await context.bot.send_message(chat_id=chat_id,
+                parse_mode=ParseMode.MARKDOWN_V2, text="_Beep\\-boop\\!_")
 
-			job_del_msg(self.app, timedelta(seconds=self.auto_delete_sec),
-				[update.message, msg])
-			return
-		try:
-			text = '?'
-			query = query[0].split(maxsplit=1)
-			action = query[0]
-			if action == 'register':
-				public_chat_id = int(query[1].replace('\\', ''))
-				client['public'] = public_chat_id
-				self.new_client(chat_id)
-				text = helpers.escape_markdown(f"""
-Register successful: {public_chat_id}
+            job_del_msg(self.app, timedelta(seconds=self.auto_delete_sec),
+                [update.message, msg])
+            return
+        try:
+            text = '?'
+            query = query[0].split(maxsplit=1)
+            action = query[0]
+            if action == 'register':
+                public_chat_id = int(query[1].replace('\\', ''))
+                client['public'] = public_chat_id
+                if client['disabled']:
+                    text = helpers.escape_markdown(f"""
+Registered with public channel: {public_chat_id}
+Alive monitoring is currently disabled.
+""", 2)
+                else:
+                    self.new_client(chat_id)
+                    text = helpers.escape_markdown(f"""
+Registered with public channel: {public_chat_id}
 See you again in {round(timedelta(seconds=self.interval).total_seconds()/60.0/60.0)} hours!
 """, 2)
-			elif action == 'cancel':
-				del client['public']
-				text = helpers.escape_markdown(f"Unregistered.", 2)
-			elif action == 'custom':
-				if len(query) < 2:
-					del client['custom']
-				else:
-					client['custom'] = query[1]
-				return await self.public_broadcast(chat_id, chat_id)
-			elif action == 'test':
-				return await self.public_broadcast(chat_id, chat_id)
-			else:
-				raise Exception("help")
-		except:
-			return await self.help(update, context)
-		await context.bot.send_message(chat_id=chat_id,
-			parse_mode=ParseMode.MARKDOWN_V2, text=text)
+            elif action == 'cancel':
+                del client['public']
+                text = helpers.escape_markdown(f"Deregistered.", 2)
+            elif action == 'enable':
+                del client['disabled']
+                if not client['public']:
+                    text = helpers.escape_markdown(f"Enabled, but no public broadcast channel registered.", 2)
+                else:
+                    self.new_client(chat_id)
+                    public_chat_id = int(client['public'])
+                    text = helpers.escape_markdown(f"""
+Enabled with public channel: {public_chat_id}
+See you again in {round(timedelta(seconds=self.interval).total_seconds()/60.0/60.0)} hours!
+""", 2)
+            elif action == 'disable':
+                client['disabled'] = True
+                text = helpers.escape_markdown(f"Disabled.", 2)
+            elif action == 'custom':
+                if len(query) < 2:
+                    del client['custom']
+                else:
+                    client['custom'] = query[1]
+                return await self.public_broadcast(chat_id, chat_id)
+            elif action == 'test':
+                return await self.public_broadcast(chat_id, chat_id)
+            else:
+                raise Exception("help")
+        except:
+            return await self.help(update, context)
+        await context.bot.send_message(chat_id=chat_id,
+            parse_mode=ParseMode.MARKDOWN_V2, text=text)
 
-	async def help(self, update, context):
-		await context.bot.send_message(
-			chat_id=update.effective_chat.id,
-			parse_mode=ParseMode.MARKDOWN_V2,
-			text=f"""
+    async def help(self, update, context):
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            parse_mode=ParseMode.MARKDOWN_V2,
+            text=f"""
 _Beep\\-boop\\!_ Alive monitor\\.
 This bot will send a check in message to you every *{round(timedelta(seconds=self.interval).total_seconds()/60.0/60.0)} hours*\\.
 After *{round(timedelta(seconds=self.public_interval+self.public_wait).total_seconds()/60.0/60.0)} hours*, a broadcast message will be send out to specified public chat\\.
 
 Commands:
+
 `/alive register <chat_id>`
 Register check in monitoring for you, send public broadcast to `<chat_id>`
 `/alive cancel`
-Cancel check in monitoring
-`/alive`
-Check in now
-`/alive test`
-Test public broadcast message in private chat
+Cancel check in monitoring, and deregister public broadcast channel
+`/alive disable`
+Disable check in monitoring, but don't deregister public broadcast channel
+`/alive enable`
+Re\\-enable check in monitoring
+
 `/alive custom <message\\.\\.\\.>`
 Append `<message...>` to public broadcast message
+`/alive test`
+Test public broadcast message in private chat
+
+`/alive`
+Check in now
 """.strip())
 
 def register(app):
-	AliveBot(app)
+    AliveBot(app)
--- services.py
+++ services.py
@@ -4,130 +4,132 @@ import mysql.connector
 __all__ = ['Service', 'nocommit']
 
 class Database:
-	# Reconnection timeout
-	timeout = timedelta(seconds=300)
+    # Reconnection timeout
+    timeout = timedelta(seconds=300)
 
-	def __init__(self):
-		self.last = None
+    def __init__(self):
+        self.last = None
 
-	def execute(self, query, params):
-		now = datetime.now(tz=timezone.utc)
-		if not self.last or (now - self.last) > self.timeout:
-			import database
-			self.db = mysql.connector.connect(
-				host = database.dbhost,
-				user = database.dbuser,
-				password = database.dbpw,
-				database = database.dbname,
-				use_unicode = True,
-				autocommit = True,
-			)
-			del database
-			self.cursor = self.db.cursor(prepared=True)
-			self.last = now
+    def execute(self, query, params):
+        now = datetime.now(tz=timezone.utc)
+        if not self.last or (now - self.last) > self.timeout:
+            import database
+            self.db = mysql.connector.connect(
+                host = database.dbhost,
+                user = database.dbuser,
+                password = database.dbpw,
+                database = database.dbname,
+                use_unicode = True,
+                autocommit = True,
+            )
+            del database
+            self.cursor = self.db.cursor(prepared=True)
+            self.last = now
 
-		try:
-			return self.cursor.execute(query, params)
-		except:
-			# Retry once
-			self.db.reconnect()
-		return self.cursor.execute(query, params)
+        try:
+            return self.cursor.execute(query, params)
+        except:
+            # Retry once
+            self.db.reconnect()
+        return self.cursor.execute(query, params)
 
-	def fetchone(self):
-		return self.cursor.fetchone()
+    def fetchone(self):
+        return self.cursor.fetchone()
 
-	def fetchall(self):
-		return self.cursor.fetchall()
+    def fetchall(self):
+        return self.cursor.fetchall()
 
 dbcursor = Database()
 
 class Service:
-	def __init__(self, service_name):
-		self.name = service_name
+    def __init__(self, service_name):
+        self.name = service_name
 
-	def __getitem__(self, client_name):
-		return Client(self, client_name)
+    def __getitem__(self, client_name):
+        return Client(self, client_name)
 
-	def __delitem__(self, client_name):
-		if type(client_name) == Client:
-			client_name = client_name.name
-		dbcursor.execute(
-			'DELETE FROM `services` WHERE `service` = %s AND `client` = %s',
-			(self.name, client_name))
+    def __delitem__(self, client_name):
+        if type(client_name) == Client:
+            client_name = client_name.name
+        dbcursor.execute(
+            'DELETE FROM `services` WHERE `service` = %s AND `client` = %s',
+            (self.name, client_name))
 
-	def __iter__(self):
-		dbcursor.execute(
-			'SELECT DISTINCT `client` FROM `services` WHERE `service` = %s',
-			(self.name,))
-		return [Client(self, row[0]) for row in dbcursor.fetchall()].__iter__()
+    def __iter__(self):
+        dbcursor.execute(
+            'SELECT DISTINCT `client` FROM `services` WHERE `service` = %s',
+            (self.name,))
+        return [Client(self, row[0]) for row in dbcursor.fetchall()].__iter__()
 
 class Client:
-	def __init__(self, service, client_name):
-		self.service = service
-		self.name = client_name
+    def __init__(self, service, client_name):
+        self.service = service
+        self.name = client_name
 
-	def __getitem__(self, key):
-		dbcursor.execute(
-			'SELECT `value` FROM `services` WHERE `service` = %s AND `client` = %s AND `key` = %s',
-			(self.service.name, self.name, key))
-		result = dbcursor.fetchone()
-		if result == None:
-			return None
-		return result[0]
+    def __getitem__(self, key):
+        dbcursor.execute(
+            'SELECT `value` FROM `services` WHERE `service` = %s AND `client` = %s AND `key` = %s',
+            (self.service.name, self.name, key))
+        result = dbcursor.fetchone()
+        if result == None:
+            return None
+        return result[0]
 
-	def __setitem__(self, key, value):
-		dbcursor.execute(
-			'INSERT INTO `services` (`service`, `client`, `key`, `value`) VALUES (%s, %s, %s, %s) ON DUPLICATE KEY UPDATE `value` = VALUE(`value`)',
-			(self.service.name, self.name, key, value))
+    def __setitem__(self, key, value):
+        if not isinstance(value, bytearray):
+            value = str(value)
+        dbcursor.execute(
+            'INSERT INTO `services` (`service`, `client`, `key`, `value`) VALUES (%s, %s, %s, %s) ON DUPLICATE KEY UPDATE `value` = VALUE(`value`)',
+            (self.service.name, self.name, key, value))
 
-	def __delitem__(self, key):
-		dbcursor.execute(
-			'DELETE FROM `services` WHERE `service` = %s AND `client` = %s AND `key` = %s',
-			(self.service.name, self.name, key))
+    def __delitem__(self, key):
+        dbcursor.execute(
+            'DELETE FROM `services` WHERE `service` = %s AND `client` = %s AND `key` = %s',
+            (self.service.name, self.name, key))
 
-	def __iter__(self):
-		dbcursor.execute(
-			'SELECT `key` FROM `services` WHERE `service` = %s AND `client` = %s',
-			(self.service.name, self.name))
-		return [row[0] for row in dbcursor.fetchall()].__iter__()
+    def __iter__(self):
+        dbcursor.execute(
+            'SELECT `key` FROM `services` WHERE `service` = %s AND `client` = %s',
+            (self.service.name, self.name))
+        return [row[0] for row in dbcursor.fetchall()].__iter__()
 
 def nocommit():
-	# Only for testing, no need to commit
-	dbcursor.db.autocommit = False
+    # Only for testing, no need to commit
+    dbcursor.db.autocommit = False
 
 def main():
-	nocommit()
+    nocommit()
 
-	service = Service('test')
-	client1 = service['client1']
-	client2 = service['client2']
+    service = Service('test')
+    client1 = service['client1']
+    client2 = service['client2']
 
-	client1['key1'] = b'value1'
-	value = client1['key1']
-	if value != b'value1':
-		raise Exception(f'wrong value: {value}')
+    client1['key1'] = b'value1'
+    value = client1['key1']
+    if value != b'value1':
+        raise Exception(f'wrong value: {value}')
 
-	client2['key2'] = b'value2'
-	value = client2['key2']
-	if value != b'value2':
-		raise Exception(f'wrong value: {value}')
+    client2['key2'] = b'value2'
+    value = client2['key2']
+    if value != b'value2':
+        raise Exception(f'wrong value: {value}')
 
-	print(f'Clients: {[client for client in service]}')
-	print(f'Client client1 Keys: {[key for key in client1]}')
-	print(f'Client client2 Keys: {[key for key in client2]}')
+    print(f'Clients: {[client for client in service]}')
+    print(f'Client client1 Keys: {[key for key in client1]}')
+    print(f'Client client2 Keys: {[key for key in client2]}')
 
-	del client1['key1']
-	value = client1['key1']
-	if value != None:
-		raise Exception(f'wrong value: {value}')
+    del client1['key1']
+    value = client1['key1']
+    if value != None:
+        raise Exception(f'wrong value: {value}')
 
-	for c in service:
-		del service[c]
+    for c in service:
+        del service[c]
 
-	print(f'Clients deleted: {[client for client in service]}')
+    print(f'Clients deleted: {[client for client in service]}')
 
-	print('all ok')
-	return 0
+    print('all ok')
+    return 0
 
 if __name__ == '__main__':
-	main()
+    main()
